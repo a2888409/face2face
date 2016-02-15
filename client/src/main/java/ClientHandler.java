bcaa17800cf6271165bbbd3f729378f1543df14b
qianzhenyu <122274659@qq.com>
2016-02-15 17:46:11 +0800

    client模块加入客户端处理函数

--- client/src/main/java/ClientHandler.java
+++ client/src/main/java/ClientHandler.java
@@ -1,5 +1,10 @@
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import protobuf.generate.cli2srv.login.Auth;
+
+import java.nio.ByteBuffer;
 
 /**
  * Created by Dell on 2016/2/15.
@@ -12,7 +17,21 @@ public class ClientHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        //ctx.write(msg);
+        Auth.CLogin.Builder loginInfo = Auth.CLogin.newBuilder();
+        loginInfo.setUserId(1);
+        loginInfo.setToken("123");
+        loginInfo.setPlatform("ios");
+        loginInfo.setAppVersion("1.0.0");
+
+        byte[] bytes = loginInfo.build().toByteArray();
+        int length = bytes.length;
+
+        ByteBuf buf = Unpooled.buffer(8 + length);
+        buf.writeInt(length);
+        buf.writeInt(1000);     //协议号
+        buf.writeBytes(bytes);
+
+        ctx.writeAndFlush(buf);
     }
 
     @Override
