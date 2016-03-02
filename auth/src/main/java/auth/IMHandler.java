3a42e87f2cc8bee8ded199a187c6bdebb1ca291c
qianzhenyu <122274659@qq.com>
2016-03-02 18:16:34 +0800

    加入逻辑处理函数的基类

---
+++ auth/src/main/java/auth/IMHandler.java
@@ -0,0 +1,21 @@
+package auth;
+
+import com.google.protobuf.Message;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * Created by Dell on 2016/3/2.
+ */
+public class IMHandler {
+    protected final String _userid;
+    protected final long  _netid;
+    protected final Message _msg;
+    protected ChannelHandlerContext _ctx;
+
+    protected IMHandler(String userid, long netid, Message msg, ChannelHandlerContext ctx) {
+        _userid = userid;
+        _netid = netid;
+        _msg = msg;
+        _ctx = ctx;
+    }
+}
---
+++ auth/src/main/java/auth/Woker.java
@@ -0,0 +1,8 @@
+package auth;
+
+/**
+ * Created by Dell on 2016/3/2.
+ */
+public class Woker extends Thread {
+    //ConcurrentHashMap<>
+}
