793357b08b8334dcc896a3ab511b9647549ab500
qianzhenyu <122274659@qq.com>
2016-04-20 22:00:48 +0800

    加入一个AIO测试客户端

---
+++ client/src/main/java/aioclient/AIOClient.java
@@ -0,0 +1,32 @@
+package aioclient;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import protobuf.ParseRegistryMap;
+
+import java.io.IOException;
+
+/**
+ * Created by win7 on 2016/4/20.
+ */
+public class AIOClient {
+    private static final Logger logger = LoggerFactory.getLogger(AIOClient.class);
+
+    public static void main(String[] args) throws IOException {
+        //init Registry
+        ParseRegistryMap.initRegistry();
+
+        for(int i = 1; i < 2; i++) {
+            AIOClientSingle client = new AIOClientSingle();
+            logger.info("AIOClient {} has been created", i);
+            //new Thread(() -> GateServer.startGateServer(gateListenPort)).start();
+            client.createSocket(Integer.parseInt(args[0]));
+        }
+
+        try {
+            Thread.sleep(1000000l);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- protobuf/src/main/java/protobuf/analysis/HandlerMap.java
+++
@@ -1,7 +0,0 @@
-package protobuf.analysis;
-
-/**
- * Created by Dell on 2016/2/1.
- */
-public class HandlerMap {
-}
